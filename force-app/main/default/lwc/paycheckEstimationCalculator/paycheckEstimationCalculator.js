c1865186c5c7e3a0833cf779407a2a9d800904a9
Jimmy Turner <jimmyturnerpt@gmail.com>
2025-06-09 21:36:43 -0600

    Creation of paycheck estimation calculator

---
+++ force-app/main/default/lwc/paycheckEstimationCalculator/paycheckEstimationCalculator.js
@@ -0,0 +1,16 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import SALARY_FIELD from '@salesforce/schema/Job_Application__c.Salary__c';
+
+export default class PaycheckEstimationCalculator extends LightningElement {
+
+    @api recordId;
+
+    @wire(getRecord, {recordId: '$recordId', fields: [SALARY_FIELD]})
+    jobApplication;
+
+    get salary() {
+        return this.jobApplication?.data?.fields?.Salary__c?.value;
+    }
+
+}
