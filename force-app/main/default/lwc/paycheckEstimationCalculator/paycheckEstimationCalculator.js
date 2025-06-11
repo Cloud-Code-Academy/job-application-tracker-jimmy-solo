b751b96b1d8710bc2dec1072e93ff3af5b59d632
Jimmy Turner <jimmyturnerpt@gmail.com>
2025-06-11 11:19:57 -0600

    Put the final touches on the paycheck estimator

--- force-app/main/default/lwc/paycheckEstimationCalculator/paycheckEstimationCalculator.js
+++ force-app/main/default/lwc/paycheckEstimationCalculator/paycheckEstimationCalculator.js
@@ -2,6 +2,9 @@ import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import SALARY_FIELD from '@salesforce/schema/Job_Application__c.Salary__c';
 
+const SOCIAL_SECURITY_RATE = 0.062;
+const MEDICARE_TAX_RATE = 0.0145;
+
 export default class PaycheckEstimationCalculator extends LightningElement {
 
     @api recordId;
@@ -27,25 +30,49 @@ export default class PaycheckEstimationCalculator extends LightningElement {
             return 0.00;
         }
     }
-    get federalTaxRatePercent(){
-        return (this.federalTaxRateDecimal * 100).toFixed(0) + '%';
-    }
+    //::::::::::::::::::START OF Calculating Values:::::::::::::::::::::://
     get yearlyEstimatedPaycheck(){
-        return this.salary - (this.salary * this.federalTaxRateDecimal);
+        return this.salary - this.totalTaxLiability;
     }
     get monthlyEstimatedPaycheck(){
         return this.yearlyEstimatedPaycheck / 12;
     }
+    get formattedYearlyEstimatedSalary(){
+        return '$' + new Intl.NumberFormat('en-US').format(this.salary - (this.salary * this.federalTaxRateDecimal));
+    }
+    get totalTaxLiability(){
+        return (this.salary * this.federalTaxRateDecimal) + (this.salary * SOCIAL_SECURITY_RATE) + (this.salary * MEDICARE_TAX_RATE);
+    }
+    //::::::::::::::::::END OF Calculating Values:::::::::::::::::::::://
+
+
+    //::::::::::::::::::START OF Formatting Values:::::::::::::::::::::://
     get formattedMonthlySalaryValue() {
         return '$' + new Intl.NumberFormat('en-US', {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2
         }).format(this.monthlyEstimatedPaycheck);
     }
+    get formattedYearlyMedicareLiability(){
+        return '$' + new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(this.salary * MEDICARE_TAX_RATE);
+    }
+    get formattedYearlyFederalTaxLiability(){
+        return '$' + new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(this.salary * this.federalTaxRateDecimal);
+    }
+    get formattedYearlySSLiability(){
+        return '$' + new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(this.salary * SOCIAL_SECURITY_RATE);
+    }
     get formattedSalaryAmount(){
         return '$' + new Intl.NumberFormat('en-US').format(this.salary);
     }
-    get formattedYearlyEstimatedSalary(){
-        return '$' + new Intl.NumberFormat('en-US').format(this.salary - (this.salary * this.federalTaxRateDecimal));
-    }
+    //::::::::::::::::::END OF Formatting Values:::::::::::::::::::::://
 }
